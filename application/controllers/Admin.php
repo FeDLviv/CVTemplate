30ae46dfcc54222fb10ecf115273bac6bed079db
FeDLviv <fed.lviv@gmail.com>
2017-11-12 21:27:00 +0200

    Add empty files(controller-Admin.php & moedl-Admin_model.php)

---
+++ application/controllers/Admin.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->model('admin_model');
+    }
+
+    public function _remap()
+    {
+        //admin_model
+        if ($this->session->test == 'my') {
+            $this->session->unset_userdata('test');
+            //$this->session->sess_destroy();
+            $this->index();
+        } else {
+            $this->session->set_userdata(['test'=>'my']);
+            $this->login();
+        }
+    }
+
+    public function index()
+    {
+        echo "INDEX";
+    }
+
+    public function login()
+    {
+        echo "LOGIN";
+    }
+
+    public function authentication()
+    {
+    }
+
+    public function ajax_logout()
+    {
+    }
+}
